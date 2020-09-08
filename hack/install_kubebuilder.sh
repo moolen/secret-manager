0492d18b0e2424c0bd1a3113348f6baa2c9642c5
Moritz Johner <beller.moritz@googlemail.com>
2020-09-08 10:18:42 +0200

    fix: remove stuff

--- hack/install_kubebuilder.sh
+++ hack/install_kubebuilder.sh
@@ -8,7 +8,5 @@ curl -L https://go.kubebuilder.io/dl/2.3.1/${os}/${arch} | tar -xz -C /tmp/
 export TARGET=${HOME}/kubebuilder
 mkdir -p ${TARGET}/
 mv /tmp/kubebuilder_2.3.1_${os}_${arch}/* ${TARGET}/
-ls -la ${TARGET}
 ls -la ${TARGET}/bin
-ls -la /home/runner/kubebuilder/bin
 export PATH=$PATH:$TARGET/bin
