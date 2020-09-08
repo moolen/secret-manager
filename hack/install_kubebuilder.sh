8b822acc8450622355f3eeae521a8084da1138d1
Moritz Johner <beller.moritz@googlemail.com>
2020-09-08 09:17:28 +0200

    fix: kb paths

--- hack/install_kubebuilder.sh
+++ hack/install_kubebuilder.sh
@@ -5,8 +5,7 @@ arch=$(go env GOARCH)
 
 curl -L https://go.kubebuilder.io/dl/2.3.1/${os}/${arch} | tar -xz -C /tmp/
 
-export TARGET=$HOME/kubebuilder
-sudo mv /tmp/kubebuilder_2.3.1_${os}_${arch}/* ${TARGET}
-ls -la ${TARGET}
-ls -la ${TARGET}/bin
+export TARGET=${HOME}/kubebuilder
+mkdir -p ${TARGET}/bin
+mv /tmp/kubebuilder_2.3.1_${os}_${arch}/* ${TARGET}/bin
 export PATH=$PATH:$TARGET/bin
