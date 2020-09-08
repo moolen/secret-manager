f1224802699599b8fa20fa23861ab0f2458efb98
Moritz Johner <beller.moritz@googlemail.com>
2020-09-08 09:47:33 +0200

    hack

--- hack/install_kubebuilder.sh
+++ hack/install_kubebuilder.sh
@@ -6,8 +6,8 @@ arch=$(go env GOARCH)
 curl -L https://go.kubebuilder.io/dl/2.3.1/${os}/${arch} | tar -xz -C /tmp/
 
 export TARGET=${HOME}/kubebuilder
-mkdir -p ${TARGET}/bin
-mv /tmp/kubebuilder_2.3.1_${os}_${arch}/* ${TARGET}/bin
+mv /tmp/kubebuilder_2.3.1_${os}_${arch}/* ${TARGET}/
+ls -la ${TARGET}
 ls -la ${TARGET}/bin
 ls -la /home/runner/kubebuilder/bin
 export PATH=$PATH:$TARGET/bin
