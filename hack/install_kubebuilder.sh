9c59f769fd7909866095931e00dc77f9194dbea2
Moritz Johner <beller.moritz@googlemail.com>
2020-09-08 10:22:48 +0200

    fix: remove travis, add gh actions

--- hack/install_kubebuilder.sh
+++ hack/install_kubebuilder.sh
@@ -5,6 +5,8 @@ arch=$(go env GOARCH)
 
 curl -L https://go.kubebuilder.io/dl/2.3.1/${os}/${arch} | tar -xz -C /tmp/
 
-TARGET=${KUBEBUILDER_ASSETS:-/usr/local/kubebuilder}
-sudo mv /tmp/kubebuilder_2.3.1_${os}_${arch}/* ${TARGET}
-export PATH=$PATH:/usr/local/kubebuilder/bin
+export TARGET=${HOME}/kubebuilder
+mkdir -p ${TARGET}/
+mv /tmp/kubebuilder_2.3.1_${os}_${arch}/* ${TARGET}/
+ls -la ${TARGET}/bin
+export PATH=$PATH:$TARGET/bin
